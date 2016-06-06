0f643506702f02c739a273463aee80e1fdec71bb
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2016-06-06 17:25:35 +0200

    The pageLoader is redesigned. Instead of switch I use the PageElementMap hash, that let us find the Controller that loads the appropriate page elements.

--- productMods/js/moduleLoaders/pageLoader.js
+++ productMods/js/moduleLoaders/pageLoader.js
@@ -1,40 +1,12 @@
 var pageLoader = {
 
-	init : function() {
-		
-		this.initData()
-		var pageModules = html.getNewDiv()
-		/*
+	loadPage : function() {
+		var pageElementBuffer = []
 		$.each(pageElements, function(index, element) {
-			switch (element.type) {
-			case "table":
-					console.log(element.dataKey)
-					var table = new TableLoader(element)
-					element.table = table
-					pageModules.append(table.getContainer())
-				break
-			case "treeStructure":
-				pageModules.append(
-					treeLoader.showTree1(element, treeStructure))
-				break
-			case "treeStructureSingle":
-				pageModules.append(
-					treeLoader.showTree2(element, treeStructure))
-				break
-			case "subPage":
-				//Normally a page data object were passed but right now the bone editor
-				//definition is not constant
-				var boneEditor = new BoneEditor(element)
-				UIController.modules[element.id] = boneEditor
-				pageModules.append(boneEditor.fullScreenContainer)
-				//This is just the loading of the frame. It "constructor" shows the sub-
-				//page module and fills it with data
-				break
-			default:
-				break
-			}
+			console.log(element.type)
+			pageElementBuffer.push(new PageElementMap[element.type](element).container)
 		})
-		$("#pageContainer").append(pageModules)*/
+		$("#pageContent").append(pageElementBuffer)
 	},
 	
 	initData : function(){
@@ -62,10 +34,5 @@ var pageLoader = {
 		})
 	}
 }
-
-$(document).ready(function() {
-	pageLoader.init()
-	
-})
 
 
