02655f872da60e15c100680b8d9dbf226bbf205c
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2016-06-23 23:54:19 +0200

    sw prefix file

---
+++ productMods/js/pageInit/SWMAP.js
@@ -0,0 +1,18 @@
+var swPrefix = "http://softwareOntology.com/"
+var sw = {
+		container : swPrefix + "Container",
+		tabContainer :  swPrefix + "TabContainer",
+		tab : swPrefix + "Tab",
+		dataTable : swPrefix + "DataTable",
+		addNew : swPrefix + "AddNew",
+		dataField : swPrefix + "DataField",
+		literalField : swPrefix + "LiteralField",
+		editButton : swPrefix + "EditButton",
+		linkDataTable : swPrefix + "LinkDataTable",
+		selectorAddField : swPrefix + "SelectorAddField",
+		global : swPrefix + "global",
+		local : swPrefix + "local",
+		constant : swPrefix + "Constant",
+		selectOperationResult : swPrefix + "selectOperationResult",
+		
+}
