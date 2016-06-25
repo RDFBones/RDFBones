1c0e8a9e56b783fac59e32fab039c0dfd4e4c3c1
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2016-06-25 15:57:24 +0200

    The page initiator algorithm is clearer

---
+++ productMods/js/pageInit/PageInit.js
@@ -0,0 +1,33 @@
+
+var PageInit = function() {
+
+	this.counter = 0;
+	this.next()
+}
+
+PageInit.prototype = {
+
+	done : function() {
+		this.counter++
+		this.next()
+	},
+
+	next : function() {
+
+		switch (this.counter) {
+
+		case 0:
+			new PageElementLoader(this)
+			break;
+		case 1: 
+			new PageDataLoader(this)
+			break;
+		case 2: 
+			DataOperation.perform(this)
+			break;
+		case 3: 
+			PageAssembler.loadPage()
+			break;
+		}
+	}
+}
