82cc6bb1eef071118a01307e6eaa4deb6d92f27f
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2016-06-25 15:50:23 +0200

    DataLoader was rewritten so that the boneDivisions are loaded properly. And as well the labels of the individuals can be queried.

--- src/edu/cornell/mannlib/vitro/webapp/search/controller/DataLoader.java
+++ src/edu/cornell/mannlib/vitro/webapp/search/controller/DataLoader.java
@@ -36,19 +36,25 @@ public class DataLoader extends VitroAjaxController {
     List<Map<String, String>> result = new ArrayList<Map<String, String>>();
     this.vreq = vreq;
     
-    log.info("itt");
     switch (vreq.getParameter("queryType")) {
-
+    
     case "coherentBones":
-      String[] inputParam = {"inputClassUri"};
-      String[] uris = {"inputClass", "boneDivision", "subClass"};
-      String[] literals = {"inputClassLabel", "boneDivisionLabel", "subClassLabel"};
+      String[] inputParam = {"skeletalInventory"};
+      String[] uris = {"boneDivision", "type"};
+      String[] literals = {"boneOrganCount", "label"};
       
       result = this.performQuery(CoherentQuery, inputParam, uris, literals);
       break;
 
     default:
+    case "label":
+      String[] inputParam1= {"individual"};
+      String[] uris1 = {};
+      String[] literals1 = {"label"};
+      
+      result = this.performQuery(LabelQuery, inputParam1, uris1, literals1);
       break;
+
     }
     log.info(result.toString());
     if (result.size() > 0) {
@@ -83,24 +89,22 @@ public class DataLoader extends VitroAjaxController {
     return QueryUtils.getQueryVars(resultSet, uris, literals);
   }
   
-
-  private static String[] BoneQueryInputUris = { "skeletalInventory" };
-  private static String[] BoneQueryUris = { "boneUri", "completeness",
-      "classUri" };
-  private static String[] BoneQueryLiterals = { "label", "description", "classLabel"};
-
-  private static String[] CoherentQueryPrefixes = { "obo", "rdfs", "rdfbones",
-      "rdf", "vitro" };
-  
+  private static String LabelQuery = ""
+      + " SELECT ?label \n"
+      + " WHERE \n "
+      + "   {  \n"
+      + "    ?individual    rdfs:label   ?label . \n"
+      + "   }  \n";
   
   private static String CoherentQuery = ""
-      + "SELECT ?boneDivision (COUNT(?boneOrgan) as ?boneOrganCount) \n"
-      + " WHERE \n "  
+      + "SELECT ?boneDivision ?label ?type (COUNT(?boneOrgan) as ?boneOrganCount) \n"
+      + " WHERE \n "
       + "   { "
-      + "    ?completeness    obo:BFO_0000050       ?skeletalInventory . \n"
-      + "    ?completeness    obo:IAO_0000136       ?boneSement . \n"
+      + "    ?skeletalInventory obo:BFO_0000051  ?completeness . \n"
+      + "    ?completeness    obo:IAO_0000136       ?boneSegment . \n"
       + "    ?boneSegment     obo:regional_part_of  ?boneOrgan  . \n"
       + "    ?boneOrgan       obo:systemic_part_of  ?boneDivision  . \n"
-      + "   } GROUP BY ?boneDivision \n";
-
+      + "    ?boneDivision    rdfs:label            ?label . \n"
+      + "    ?boneDivision    vitro:mostSpecificType   ?type .  \n"  
+      + "   } GROUP BY ?boneDivision ?label ?type \n";
 }
