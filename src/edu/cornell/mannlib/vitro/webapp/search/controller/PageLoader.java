678900b67abd77b6f06acf3ae214e50035989b83
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2016-06-26 11:46:31 +0200

    ProfilePageLoader was renamed to PageLoader.

---
+++ src/edu/cornell/mannlib/vitro/webapp/search/controller/PageLoader.java
@@ -0,0 +1,59 @@
+package edu.cornell.mannlib.vitro.webapp.search.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import edu.cornell.mannlib.vitro.webapp.controller.VitroRequest;
+import edu.cornell.mannlib.vitro.webapp.controller.freemarker.FreemarkerHttpServlet;
+import edu.cornell.mannlib.vitro.webapp.controller.freemarker.responsevalues.ResponseValues;
+import edu.cornell.mannlib.vitro.webapp.controller.freemarker.responsevalues.TemplateResponseValues;
+
+public class PageLoader extends FreemarkerHttpServlet {
+
+  private static final long serialVersionUID = 1L;
+  private static final Log log = LogFactory.getLog(PageLoader.class.getName());
+
+  @Override
+  protected ResponseValues processRequest(VitroRequest vreq) {
+
+    String templateName = new String();
+    String pageUri = vreq.getParameter("pageUri");
+
+    /*
+     * Right now we just hard code the different page data configurations but
+     * later it has to be loaded from the triples.
+     */
+    switch (pageUri) {
+    case "skeletalInventory":
+      templateName = "skeletalInventory.ftl";
+      break;
+
+    case "boneDivision":
+      templateName = "boneDivision.ftl";
+      break;
+
+    case "coherentBones":
+      templateName = "coherentBones.ftl";
+      break;
+    case "boneOrgan":
+      templateName = "boneOrgan.ftl";
+
+    default:
+      break;
+    }
+
+    Map<String, String> params = new HashMap<String, String>();
+    for (String key : vreq.getParameterMap().keySet()) {
+      if (!key.equals("pageUri")) {
+        params.put(key, vreq.getParameter(key));
+      }
+    }
+
+    Map<String, Object> body = new HashMap<String, Object>();
+    body.put("params", params);
+    return new TemplateResponseValues(templateName, body);
+  }
+}
