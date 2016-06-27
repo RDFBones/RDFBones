d44e6075d0d5651322aeb82f3852d5450151fa9a
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2016-06-27 21:07:48 +0200

    There is field type LiteralField middle

--- productMods/js/UIModules/pageElements/LiteralField.js
+++ productMods/js/UIModules/pageElements/LiteralField.js
@@ -9,11 +9,11 @@ var LiteralField = function(parent, configData){
 }
 
 
-var LiteralFieldMiddle = function(fieldData, configData){
+var LiteralFieldMiddle = function(parent, configData){
 	
 	this.parent =  parent
 	//this.value = data[configData.dataKey]
-	this.container = html.div("literalField").text(getData(this, configData.value))
+	this.container = html.div("literalFieldMiddle").text(getData(this, configData.value))
 }
 
 
