9b2cd9c1fc4d34aa7c8ee4ddaf0217d844e94ad9
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2016-06-27 22:04:05 +0200

    LinkField works properly

---
+++ productMods/js/UIModules/pageElements/LinkField.js
@@ -0,0 +1,23 @@
+
+
+var LinkField = function(configData){
+	
+	this.container = html.div()
+	this.title = html.div("title").text(getData1(configData.title))
+	this.link = html.link(DataLib.assembleUrl(configData))
+	this.label = html.div("margin10").text(getData1(configData.label))
+	this.assemble()
+}
+
+LinkField.prototype = {
+	
+	assemble : function(){
+		
+		UI.assemble(this.container, [
+		    this.title,
+		    this.link,
+		    	this.label,
+		    ], [0, 0, 1])
+	}
+	
+}
