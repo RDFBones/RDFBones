e8eaaf6f780dda456abe0e489a3b3504fe23802c
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2016-06-29 19:43:25 +0200

    The config data from the the deleted two files were placed to the symmetricConfig.js. The coherentBones.ftl were deleted and replaces by the symmetricBoneDivision.ftl.

---
+++ productMods/js/customConfigData/CEF/coherentBones/symmetricConfig.js
@@ -0,0 +1,125 @@
+pageData.pageElements = [ {
+	type : sw.customPage,
+	pageLoader : ClassSelector,
+} ]
+
+pageData.queries = [ {
+	parameters : [ {
+		type : sw.global,
+		varName : "classUri",
+		key : "classUri"
+	}, {
+		type : sw.constant,
+		value : "systemicParts",
+		varName : "queryType"
+	} ],
+	mapping : "anatomicalModelLoader",
+	toVariable : "boneDivisions1",
+}, {
+	parameters : [ {
+		type : sw.global,
+		varName : "classUri",
+		key : "classUri"
+	}, {
+		type : sw.constant,
+		value : "subClasses",
+		varName : "queryType"
+	} ],
+	mapping : "anatomicalModelLoader",
+	toVariable : "boneDivisions2",
+}, {
+	
+	requirement : {
+		type : sw.existence,
+		variable : {
+			type : sw.global,
+			key : "existingBoneDivision",
+		}
+	},
+
+	parameters : [ {
+		type : sw.global,
+		varName : "boneDivision",
+		key : "existingBoneDivision"
+	}, {
+		type : sw.constant,
+		value : "boneOrgans",
+		varName : "queryType",
+	} ],
+	mapping : "dataLoader",
+	toVariable : "existingBoneOrgans",	
+} ]
+
+
+pageData.dataOperations = [
+
+{
+	type : "grouping",
+	processes : [ {
+		inputVariable : "boneDivisions1",
+		by : "inputClass",
+		within : [ "inputClassLabel" ],
+		to : "systemicParts",
+		rename : [ {
+			key : "inputClass",
+			to : "uri"
+		}, {
+			key : "inputClassLabel",
+			to : "label"
+		} ]
+	}, {
+		inputVariable : "systemicParts",
+		by : "boneDivision",
+		within : [ "boneDivisionLabel" ],
+		to : "subClasses",
+		rename : [ {
+			key : "boneDivision",
+			to : "uri"
+		}, {
+			key : "boneDivisionLabel",
+			to : "label"
+		} ]
+	}, {
+		inputVariable : "subClasses",
+		by : "subClass",
+		within : [ "subClassLabel" ],
+		rename : [ {
+			key : "subClass",
+			to : "uri"
+		}, {
+			key : "subClassLabel",
+			to : "label"
+		} ]
+	} ]
+}, {
+	type : "grouping",
+	processes : [ {
+		inputVariable : "boneDivisions2",
+		by : "boneDivision",
+		within : [ "boneDivisionLabel" ],
+		to : "systemicParts",
+		rename : [ {
+			key : "boneDivision",
+			to : "uri"
+		}, {
+			key : "boneDivisionLabel",
+			to : "label"
+		} ]
+	}, {
+		inputVariable : "systemicParts",
+		by : "systemicPart",
+		within : [ "systemicPartLabel" ],
+		to : "unused",
+		rename : [ {
+			key : "systemicPart",
+			to : "uri"
+		}, {
+			key : "systemicPartLabel",
+			to : "label"
+		} ]
+	} ]
+}, {
+	type : "mergeArrays",
+	arrays : [ "boneDivisions1", "boneDivisions2" ],
+	output : "boneDivisions"
+} ]
