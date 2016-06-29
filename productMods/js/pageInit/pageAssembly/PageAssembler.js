4f19cc4ac191a4bc359200eb896b7848df5f7d11
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2016-06-29 19:46:29 +0200

    PageAssembler do not accept this initator approach. It checks if the page element an custompage is.

--- productMods/js/pageInit/pageAssembly/PageAssembler.js
+++ productMods/js/pageInit/pageAssembly/PageAssembler.js
@@ -2,25 +2,15 @@ var PageAssembler = {
 
 	loadPage : function() {
 	
-		if(pageData.initiator != undefined){
-			
-			var obj = pageData.initiator.object
-			switch(pageData.initiator.inputs.length){
-			case 1 :
-				$("#pageContent").append(new obj(pageData[pageData.initiator.inputs[0]]).container)
-				break
-			case 2 :
-				$("#pageContent").append(new obj(pageData[pageData.initiator.inputs[0]],
-						pageData[pageData.initiator.inputs[1]]).container)
-				break
-			}
-		} else {
 			var pageElementBuffer = []
 			$.each(pageData.pageElements, function(index, element) {
-				pageElementBuffer.push(new PageElementMap[element.type](element).container)
+				if(element.type == sw.customPage){
+					pageElementBuffer.push(new element.pageLoader().container)
+				} else {
+					pageElementBuffer.push(new PageElementMap[element.type](element).container)	
+				}
 			})
 			$("#pageContent").append(pageElementBuffer)
-		}
 	},	
 }
 
