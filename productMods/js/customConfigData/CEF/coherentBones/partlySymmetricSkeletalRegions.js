c75e9e36f84cd83df0e5bf8b2644b4ac489caca2
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2016-06-30 21:44:30 +0200

    This config data defines the symmetric classes for the viscerocranium and the neurocranium.

---
+++ productMods/js/customConfigData/CEF/coherentBones/partlySymmetricSkeletalRegions.js
@@ -0,0 +1,22 @@
+//Neurocranium
+
+
+pageData.partlySymmetricBones = {
+
+	"http://purl.obolibrary.org/obo/FMA_53672" : {
+
+		parentRegions : [ "http://purl.obolibrary.org/obo/FMA_9613" ]
+	},
+
+	"http://purl.obolibrary.org/obo/FMA_53673" : {
+
+		parentRegions : [ "http://purl.obolibrary.org/obo/FMA_9711",
+		  		"http://purl.obolibrary.org/obo/FMA_54736",
+				"http://purl.obolibrary.org/obo/FMA_52737",
+				"http://purl.obolibrary.org/obo/FMA_52741",
+				"http://purl.obolibrary.org/obo/FMA_52745",
+				"http://purl.obolibrary.org/obo/FMA_52746",
+				"http://purl.obolibrary.org/obo/FMA_52747"]
+	},
+
+}
