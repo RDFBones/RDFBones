f279e85d7ead4c3f7162cc682159a54a50b3303e
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2016-07-03 22:52:48 +0200

    Configuration of the single bones

---
+++ productMods/js/customConfigData/profilePage/skeletalInventory/singleBones/singleSkull.js
@@ -0,0 +1,59 @@
+
+neurocranium = [ 
+	"http://purl.obolibrary.org/obo/FMA_52734",
+	"http://purl.obolibrary.org/obo/FMA_52735",
+	"http://purl.obolibrary.org/obo/FMA_9613",
+	"http://purl.obolibrary.org/obo/FMA_52734"]
+
+viscerocranium = [ "http://purl.obolibrary.org/obo/FMA_52740",
+		"http://purl.obolibrary.org/obo/FMA_52749",
+		"http://purl.obolibrary.org/obo/FMA_54736",
+		"http://purl.obolibrary.org/obo/FMA_52741",
+		"http://purl.obolibrary.org/obo/FMA_52748",
+		"http://purl.obolibrary.org/obo/FMA_9711",
+		"http://purl.obolibrary.org/obo/FMA_52745",
+		"http://purl.obolibrary.org/obo/FMA_52746",
+		"http://purl.obolibrary.org/obo/FMA_52736",
+		"http://purl.obolibrary.org/obo/FMA_52737",
+		"http://purl.obolibrary.org/obo/FMA_9710",
+		"http://purl.obolibrary.org/obo/FMA_52747" ]
+
+
+pageData.neurocraniumClasses = neurocranium
+pageData.viscerocraniumClasses = viscerocranium
+
+singleNeurocranium = {
+		type : sw.container,
+		localData : [
+				{
+					type : sw.constant,
+					value : "Neurocranium",
+					key : "boneLabel",
+				}, {
+					type : sw.constant,
+					value : "singleNeurocranium",
+					varName : "dataKey",
+				}],
+		elements : [ coherentData]
+	}
+
+singleViscerocranium = {
+		type : sw.container,
+		localData : [
+				{
+					type : sw.constant,
+					value : "Neurocranium",
+					key : "boneLabel",
+				}, {
+					type : sw.constant,
+					value : "singleViscerocranium",
+					varName : "dataKey",
+				}],
+		elements : [ coherentData]
+	}
+
+singleSkullTab = {
+	type : sw.tab,
+	tabTitle : "Skull",
+	elements : [singleNeurocranium, singleViscerocranium]
+}
