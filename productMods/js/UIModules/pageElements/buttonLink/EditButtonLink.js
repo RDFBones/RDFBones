24183a9eb9bcb628701a08b1aca41645419091ab
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2016-07-04 00:06:53 +0200

    DeleteButtonLink type handler class is added

--- productMods/js/UIModules/pageElements/buttonLink/EditButtonLink.js
+++ productMods/js/UIModules/pageElements/buttonLink/EditButtonLink.js
@@ -5,3 +5,35 @@ var EditButtonLink = function(parent, configData){
 }
 
 EditButtonLink.prototype = Object.create(ParentButtonLink.prototype)
+
+var DeleteButtonLink = function(parent, configData){
+	
+	this.configData = configData
+	this.parent = parent
+	this.assembleData()
+	this.container = new Button("del", (this.routine).bind(this)).container
+}
+ 
+
+DeleteButtonLink.prototype = {
+		
+	assembleData : function(){
+		data = new Object()
+		$.each(this.configData.linkDataInputs, (function(i, value){
+			data[value.key] = getData(this, value)
+		}).bind(this))
+		this.data = data
+	},	
+		
+	routine : function(){
+		
+		$.ajax({
+			url : baseUrl + "delete",
+			context : this,
+			dataType : "json",
+			data : this.data
+		}).done(function(result){
+			location.reload();
+		})
+	}
+}
