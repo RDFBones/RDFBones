9961d89ea520b87bfcabd9b87648346c830ffc50
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2016-07-12 04:20:02 +0200

    New Checkbox container element.

--- productMods/js/UIModules/generalElements/Button.js
+++ productMods/js/UIModules/generalElements/Button.js
@@ -16,21 +16,25 @@ Button.prototype = {
 	},	
 	
 	show : function(){
-		this.container.css("display", "block")
+		this.container.css("display", "inline-block")
+		return this
 	},
 	
 	hide : function(){
 		this.container.css("display","none")
+		return this
 	},
 	
 	disable : function(){
 		this.enabled = false
 		this.container.addClass("disabledImg").removeClass("enabledImg")
+		return this
 	},
 	
 	enable : function(){
 		this.enabled = true
 		this.container.removeClass("disabledImg").addClass("enabledImg")
+		return this
 	}
 }
 
@@ -48,3 +52,44 @@ var CustomButton = function(type, returnFunction, customClass){
 }
 
 CustomButton.prototype = Object.create(Button.prototype)
+
+
+var CheckBoxText = function(text, parent, selectFunction, deselectFunction){
+	
+	this.container = html.div("margin15H")
+	this.container.append(html.div("inline").text(text))
+	
+	this.checkBox = html.checkBox().change(function(){
+		if(this.checked){
+			console.log(parent)
+			parent[selectFunction](parent)
+		} else {
+			parent[deSelectFunction](parent)
+		}
+	}).addClass("inline")
+	this.container.append(this.checkBox)
+}
+
+
+CheckBoxText.prototype = {
+		
+	hide : function(){
+		this.container.hide()
+		return this
+	},
+	
+	show : function(){
+		this.container.show()
+		return this
+	},
+	
+	set : function(){
+		this.checkBox.prop('checked', true)
+		this.checkBox.attr("disabled", true)
+	},
+	
+	reset : function(){
+		this.checkBox.prop('checked', false)
+		this.checkBox.attr("disabled", false)
+	}
+}
