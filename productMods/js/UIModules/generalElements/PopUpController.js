05efd28248b72165c99b7707cf0971ba3e716b18
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2016-07-20 10:20:50 +0200

    New msg function for the popupcontroller

--- productMods/js/UIModules/generalElements/PopUpController.js
+++ productMods/js/UIModules/generalElements/PopUpController.js
@@ -19,7 +19,11 @@ var PopUpController = {
 		$("#popUpContainer").append(this.container)
 	},
 	
-	doneMsg : function(msg){
+	doneMsg : function(msg, time){
+		
+		if(time === undefined){
+			time = 1000
+		}
 		this.container.empty()
 		//This type of inner container will adapt to the image
 		this.innerContainer = UI.getFullScreenInner()
@@ -29,7 +33,22 @@ var PopUpController = {
 		this.container.append(this.innerContainer)
 		setTimeout((function(){
 			this.container.remove()
-		}).bind(this), 500)
-	}
+		}).bind(this), time)
+	},
 	
+	note : function(msg){
+		
+		this.container = html.div("popUpOuter")
+		this.innerContainer = UI.getFullScreenInner()
+				.append(html.div("verticalMiddleContainer")
+				.append(html.div("msgText").text(msg))
+				.append(ImgUI.libImg("done32", "inline margin10")))
+		this.container.append(this.innerContainer)
+		$("#popUpContainer").append(this.container)
+		setTimeout((function(){
+			this.container.remove()
+		}).bind(this), 2000)
+	}
 }
+
+
