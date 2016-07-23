26a61c97e7add2f0ea4aae109f892ffda425dd82
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2016-07-23 10:08:14 +0200

    There is only one container with the tabs of different skeletal regions.

--- productMods/js/customConfigData/profilePage/skeletalInventory/tabs.js
+++ productMods/js/customConfigData/profilePage/skeletalInventory/tabs.js
@@ -7,32 +7,4 @@ coherentTabContainer = {
 			thoraxTab, bonyPelvisTab, lowerLimbTab ]
 }
 
-coherentTab = {
-		type : sw.tab,
-		tabTitle : "Coherent Skeletal Regions",
-		elements : [ coherentTabContainer ]
-	}
-
-singleTabContainer = {
-		type : sw.tabContainer,
-		tabs : [ singleSkullTab, singleLowerLimbTab ]
-	}
-
-singleTab = {
-	type : sw.tab,
-	tabTitle : "Single Bones",
-	elements : [singleTabContainer]
-}
-
-skeletalDivision = {
-		type : sw.tab,
-		tabTitle : "Skeletal Subdivision",
-		elements : skeletalRegionElements,
-	}
-
-tabContainer = {
-	type : sw.tabContainer,
-	//tabs : [skeletalDivision, coherentTab, singleTab ],
-	tabs : [ coherentTab ]}
-
 pageData.pageElements = [ coherentTabContainer ]
