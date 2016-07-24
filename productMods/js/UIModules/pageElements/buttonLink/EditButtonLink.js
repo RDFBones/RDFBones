54c12fd8d93359b4f95aef927c7295e691f4937e
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2016-07-24 10:35:09 +0200

    No the editButtonLink controller adds the skeletalDivisionType to single bones after the deletion so that they can be identified by the skeletalInventory profile page.

--- productMods/js/UIModules/pageElements/buttonLink/EditButtonLink.js
+++ productMods/js/UIModules/pageElements/buttonLink/EditButtonLink.js
@@ -13,14 +13,17 @@ var DeleteButtonLink = function(parent, configData){
 	this.assembleData()
 	this.container = new Button("del", (this.routine).bind(this)).container
 }
- 
 
 DeleteButtonLink.prototype = {
 		
 	assembleData : function(){
 		data = new Object()
 		$.each(this.configData.linkDataInputs, (function(i, value){
-			data[value.key] = getData(this, value)
+			if(value.varName != undefined){
+				data[value.varName] = getData(this, value)	
+			} else {
+				data[value.key] = getData(this, value)	
+			}
 		}).bind(this))
 		this.data = data
 	},	
@@ -33,7 +36,41 @@ DeleteButtonLink.prototype = {
 			dataType : "json",
 			data : this.data
 		}).done(function(result){
-			location.reload();
+			
+			
+			if(this.configData.afterProcess != undefined){
+				//Perform after process. Now it is just a single version
+				$.ajax({
+					dataType : "json",
+					url : baseUrl + "dataLoader",
+					data : {
+						queryType : "singleBonesWithout",
+					}
+				}).done(function(result) {
+				
+					console.log(result)
+					$.each(result, function(i, res){
+						res.type = "existing"
+					})
+					
+					var object = new Object()
+					console.log(result)
+					object.boneOrgan = result
+					object.operation = "addSingleBoneOf"
+					object.skeletalDivisionClass = pageData.skeletalDivisionType
+					
+					$.ajax({
+						dataType : "json",
+						url : baseUrl + "dataInput",
+						data : "dataToStore=" + JSON.stringify(object)
+					}).done(function(msg){
+						location.reload()
+					})
+				})
+			} else {
+				location.reload();
+			}
+			
 		})
 	}
 }
