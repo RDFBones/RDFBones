58e9b707e510cdb075795e6f432a67af5e94ac27
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2016-09-04 21:22:32 +0200

    CoherentSkeletalDivision profile page is set to have link to its skeletal inventory.

--- productMods/js/customConfigData/profilePage/coherentSkeletalDivision/query.js
+++ productMods/js/customConfigData/profilePage/coherentSkeletalDivision/query.js
@@ -11,6 +11,44 @@ pageData.queries = [
 		] 
 
 
+pageData.skeletalInventory = {
+	dataOperation : "query",
+	queryType : "skeletalInventory1",
+	singleQuery : true,
+	resultKey : "skeletalInventory",
+	parameters : [{
+		value : {
+			type : sw.global,
+			key : "individual",
+		},
+		name : "skeletalDivision",
+	}]
+}
+
+pageData.skeletalInventoryLabel = {
+		
+	dataOperation : "query",
+	queryType : "literal",
+	type : "literal",
+	singleQuery : true,
+	parameters : [
+			{
+			value : {
+				type : sw.global,
+				key : "skeletalInventory",
+			},
+			name : "subject",
+			}, {
+				value : {
+					type : sw.constant,
+					value : "http://www.w3.org/2000/01/rdf-schema#label", 
+				},
+				name : "predicate",
+			}
+	]
+}
+
+
 pageData.type = individualType
 
 pageData.pageElements = [{
