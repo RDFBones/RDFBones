e3a3c6f08d627da3a72be9397a400fab005a1a8b
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2016-09-04 22:57:38 +0200

    The fresh bone skeletalInventory has as well the label editor field.

--- productMods/js/customConfigData/profilePage/skeletalInventory/tabs.js
+++ productMods/js/customConfigData/profilePage/skeletalInventory/tabs.js
@@ -1,10 +1,21 @@
-pageData.individual = "http://testIndividual"
-
-
 coherentTabContainer = {
 	type : sw.tabContainer,
 	tabs : [ skullTab, shoulderGridleTab, upperLimbTab, vertebralColumnTab,
 			thoraxTab, bonyPelvisTab, lowerLimbTab ]
 }
 
-pageData.pageElements = [ coherentTabContainer ]
+pageData.pageElements = [ {
+	type : sw.literalEditor,
+	title : {
+		type : sw.constant,
+		value : "Label",
+	},
+	subject : {
+		type : sw.global,
+		key : "individual",
+	},
+	predicate : {
+		type : sw.constant,
+		value : "rdfs:label",
+	}
+}, coherentTabContainer ]
