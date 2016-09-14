f790b329283a4b55ce2e09d7e152115c43374ab2
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2016-09-14 23:21:20 +0200

    The NewInstance.java does not search for the template of the individual it rederects to the defaultPageLoad link where the DefaultPageLoad.java cares about the loading.

--- src/edu/cornell/mannlib/vitro/webapp/search/controller/NewInstance.java
+++ src/edu/cornell/mannlib/vitro/webapp/search/controller/NewInstance.java
@@ -5,8 +5,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
+import rdfbones.pageload.TemplateGetter;
 
 import com.hp.hpl.jena.rdf.model.Literal;
 
@@ -18,6 +21,7 @@ import edu.cornell.mannlib.vitro.webapp.beans.IndividualImpl;
 import edu.cornell.mannlib.vitro.webapp.beans.VClass;
 import edu.cornell.mannlib.vitro.webapp.controller.VitroRequest;
 import edu.cornell.mannlib.vitro.webapp.controller.freemarker.FreemarkerHttpServlet;
+import edu.cornell.mannlib.vitro.webapp.controller.freemarker.responsevalues.DirectRedirectResponseValues;
 import edu.cornell.mannlib.vitro.webapp.controller.freemarker.responsevalues.ResponseValues;
 import edu.cornell.mannlib.vitro.webapp.controller.freemarker.responsevalues.TemplateResponseValues;
 import edu.cornell.mannlib.vitro.webapp.controller.individual.IndividualTemplateLocator;
@@ -40,7 +44,15 @@ public class NewInstance extends FreemarkerHttpServlet{
   public ResponseValues processRequest(VitroRequest vreq) throws InsertException, UnsupportedEncodingException{
     
     String classUri = vreq.getParameter("classUri");
-    String classLabel = java.net.URLDecoder.decode(vreq.getParameter("label"), "UTF-8");
+    
+    String classLabel = new String();
+    if(StringUtils.isNotEmpty(vreq.getParameter("label"))){
+      classLabel = java.net.URLDecoder.decode(vreq.getParameter("label"), "UTF-8");
+    } else {
+      DataPropertyStatementDao dataPropDao = vreq.getWebappDaoFactory().getDataPropertyStatementDao();
+      List<Literal> literals = dataPropDao.getDataPropertyValuesForIndividualByProperty(classUri, "http://www.w3.org/2000/01/rdf-schema#label");
+      classLabel = literals.get(0).getString();
+    }
     
     //Creating the new instance
     IndividualDao indDao = vreq.getWebappDaoFactory().getIndividualDao();
@@ -48,21 +60,13 @@ public class NewInstance extends FreemarkerHttpServlet{
     i.setVClassURI(classUri);
     String indUri = indDao.insertNewIndividual(i);
     
-    //Setting the label of the new instance based on the URI the classLabel
+    
+    //Setting Label
     String label = classLabel + "_" + indUri.substring(indUri.length() -3);
     DataPropertyStatementDao dpsDao = vreq.getWebappDaoFactory().getDataPropertyStatementDao();
     dpsDao.insertNewDataPropertyStatement(new DataPropertyStatementImpl(indUri, N3Utils.getOnlyPredicate("rdfs:label"), label));
     
-    //Getting the template file for the class
-    DataPropertyStatementDao dpsd = vreq.getWebappDaoFactory().getDataPropertyStatementDao();
-    List<Literal> customTemplate = dpsd.getDataPropertyValuesForIndividualByProperty(classUri, "http://vitro.mannlib.cornell.edu/ns/vitro/0.7#customDisplayViewAnnot");
-    String template = customTemplate.get(0).getString();
     
-    //DataMap for the template
-    Map<String, Object> data = new HashMap<String, Object>();
-    data.put("individual", indUri);
-    data.put("label", label);
-
-    return new TemplateResponseValues(template, data);
+    return new DirectRedirectResponseValues("defaultPageLoad?individual=" + indUri);
   }
 }
