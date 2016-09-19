cbaf7c66889824420352237c367d3935ca9c1cee
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2016-09-19 18:19:39 +0200

    The template evalutaion based on individualUri happens through the TemplateGetter.

--- src/rdfbones/pageload/DefaultPageLoad.java
+++ src/rdfbones/pageload/DefaultPageLoad.java
@@ -40,58 +40,8 @@ public class DefaultPageLoad extends FreemarkerHttpServlet{
     //Getting the input parameters
     String individualUri = vreq.getParameter("individual");
     
-    /*
-     * @@@ To solve @@@
-     * Iteratively check the superclasses of the individual if they
-     * have custom template
-     */
-    
-    /*
-     * @@@ Thesis related note @@@
-     * In the final version, the Page instance
-     * will be called by the Servlet, which get each configuration
-     * data of the found page. 
-     */
-
-    //Getting the classUri
-    log.info(individualUri);
     //Daos for template
-    IndividualDao iDao = vreq.getWebappDaoFactory().getIndividualDao();
-    Individual ind = iDao.getIndividualByURI(individualUri);
-    //Dao for template
-    DataPropertyStatementDao dpsd = vreq.getWebappDaoFactory().getDataPropertyStatementDao();
-    //Getting type of the individual
-    List<ObjectPropertyStatement> ops = ind.getObjectPropertyStatements(N3Utils.getOnlyPredicate("vitro:mostSpecificType"));
-    String classUri = ops.get(0).getObjectURI();
-    
-    //List<VClass> vclasses = ind.getVClasses(true);
-    //String classUri1 = vclasses.get(0).getURI();
-
-    String template = new String();
-    //Dao for class
-    VClassDao vdao = vreq.getWebappDaoFactory().getVClassDao();
-    List<String> superClasses = new ArrayList<String>();
-    superClasses.add(classUri);
-    while(true){
-      classUri = this.getSuperClass(classUri, vdao);
-      if(StringUtils.isEmpty(classUri)){
-        break;
-      } else {
-        superClasses.add(classUri);
-      }
-    }
-    
-    for(String uri : superClasses){
-      List<Literal> customTemplates = dpsd.getDataPropertyValuesForIndividualByProperty(uri, "http://vitro.mannlib.cornell.edu/ns/vitro/0.7#customDisplayViewAnnot");
-      if(customTemplates.size() > 0){
-        template = customTemplates.get(0).getString();
-        break;
-      }
-    }
-
-    if(StringUtils.isEmpty(template)){
-      template = "noTemplate.ftl";
-    }
+    String template = TemplateGetter.getTemplateByIndividual(individualUri, vreq);
     
     Map<String, Object> data = new HashMap<String, Object>();
     Map<String, Object> params = new HashMap<String, Object>();
@@ -99,22 +49,5 @@ public class DefaultPageLoad extends FreemarkerHttpServlet{
     data.put("params", params);
     
     return new TemplateResponseValues(template, data);
-  }
-  
-  public String getSuperClass(String classUri, VClassDao vdao){
-
-    String toReturn = new String("");
-    List<String> superClasses = vdao.getSuperClassURIs(classUri, true);
-    for(String uri : superClasses){
-      VClass vc = vdao.getVClassByURI(uri);
-      if(vc != null){
-        if(!vc.getLabel().startsWith("restriction")){
-          log.info(vc.getLabel());
-          toReturn = vc.getURI();
-          break;
-        }
-      }
-    }
-    return toReturn;
   }
 }
