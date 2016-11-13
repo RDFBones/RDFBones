374625912a356846f335de7cba4462dfbeebaff9
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2016-11-13 23:26:00 +0100

    WebappConnector is placed into the webappconnector package.

--- src/webappconnector/WebappConnector.java
+++ src/webappconnector/WebappConnector.java
@@ -1,9 +1,8 @@
-package rdfbones.lib;
+package webappconnector;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -13,26 +12,30 @@ import edu.cornell.mannlib.vitro.webapp.dao.InsertException;
 import edu.cornell.mannlib.vitro.webapp.dao.NewURIMakerVitro;
 import edu.cornell.mannlib.vitro.webapp.dao.jena.QueryUtils;
 import edu.cornell.mannlib.vitro.webapp.edit.n3editing.VTwo.NewURIMaker;
-
-public class WebappConnector {
-
+ 
+ public class WebappConnector {
+ 
   private Log log = LogFactory.getLog(WebappConnector.class); 
 
   public Map<String, Object> requestMap = new HashMap<String, Object>();
-  public boolean logEnabled = true;
   VitroRequest vreq;
   NewURIMaker newURIMaker;
-  
+  boolean logEnabled = true;
+   
   public WebappConnector(VitroRequest vreq){
     this.vreq = vreq;
     newURIMaker = new NewURIMakerVitro(vreq.getWebappDaoFactory());
   }
   
-  public WebappConnector(boolean log){
-    this.logEnabled = log;
-  }
-  
-  public String getUnusedNewURI(){
+  public WebappConnector(){
+    
+   }
+   
+   public WebappConnector(boolean log){
+     this.logEnabled = log;
+   }
+   
+   public String getUnusedNewURI(){
 
     try {
       return newURIMaker.getUnusedNewURI(null);
@@ -40,26 +43,21 @@ public class WebappConnector {
       e.printStackTrace();
     }
     return new String("");
-  }
-  
-  public String getInputParameter(String parameterName){
-    
-    if(this.requestMap.containsKey(parameterName)){
-      return this.requestMap.get(parameterName).toString();
-    } else {
-      return parameterName + "URI";  
-    }
-  }
-  
-  public List<Map<String, String>> sparqlResult(String queryStr, List<String> uris, List<String> literals){
-    
-    return QueryUtils.getResult(queryStr, uris, literals, this.vreq);
-  }
-  
-  public void log(String msg){
-    
-    if(this.logEnabled){
+   }
+   
+   public String getInputParameter(String parameterName){
+     return vreq.getParameter(parameterName);
+   }
+   
+   public List<Map<String, String>> sparqlResult(String queryStr, List<String> uris, List<String> literals){
+     
+      return QueryUtils.getResult(queryStr, uris, literals, this.vreq);
+   }
+   
+   public void log(String msg){
+     
+     if(this.logEnabled){
       log.info(msg);
-    }
-  }
-}
+     }
+   }
+ }
