6ca267cab0ef289addbbb937b1e89fe646a0dcb6
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2016-12-01 20:12:48 +0100

    EntryForm generator classes for skeletal inventories.

---
+++ src/rdfbones/generators/SkeletalRegionGenerator.java
@@ -0,0 +1,22 @@
+package rdfbones.generators;
+
+
+import javax.servlet.http.HttpSession;
+
+import rdfbones.lib.RDFBonesUtils;
+import rdfbones.lib.TripleLib;
+import webappconnector.VIVOWebappConnector;
+import edu.cornell.mannlib.vitro.webapp.controller.VitroRequest;
+import edu.cornell.mannlib.vitro.webapp.edit.n3editing.VTwo.EditConfigurationVTwo;
+import edu.cornell.mannlib.vitro.webapp.edit.n3editing.configuration.generators.EditConfigurationGenerator;
+
+public class SkeletalRegionGenerator implements EditConfigurationGenerator {
+
+  @Override
+  public EditConfigurationVTwo getEditConfiguration(VitroRequest vreq,
+    HttpSession session) throws Exception {
+
+    return RDFBonesUtils.getEditConfiguration(vreq, TripleLib.srDataTriples(), 
+        TripleLib.srSchemeTriples(), TripleLib.srForm(), new VIVOWebappConnector(vreq));
+  }
+}
