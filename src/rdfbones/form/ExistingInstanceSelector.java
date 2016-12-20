8cebb57931ab62e614247b003fb9a6c36bb80caf
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2016-12-20 22:46:38 +0100

    ExistingInstanceSelector class now return its JSON descriptor as well.

--- src/rdfbones/form/ExistingInstanceSelector.java
+++ src/rdfbones/form/ExistingInstanceSelector.java
@@ -1,9 +1,29 @@
 package rdfbones.form;
 
+import org.json.JSONObject;
+
+import rdfbones.graphData.FormGraph;
+import rdfbones.lib.JSON;
+
 public class ExistingInstanceSelector extends FormElement {
 
   public ExistingInstanceSelector(String varName, String title){
     super(varName, title);
-    this.type = new String("adder");
+    this.type = new String("existingInstanceSelector");
+  }
+  
+  @Override
+  public JSONObject getDescriptor(FormConfiguration formConfig){
+
+    JSONObject object = JSON.obj();
+    JSON.put(object, "title", this.title);
+    JSON.put(object, "type", this.type);
+    JSON.put(object, "style", this.style);
+    JSON.put(object, "dataKey", this.dataKey);
+    if(formConfig.formGraphs.containsKey(this.dataKey)){
+    	FormGraph formGraph = formConfig.formGraphs.get(this.dataKey);
+    	JSON.put(object, "table", formGraph.table.getDescriptor());
+    }
+    return object;
   }
 }
