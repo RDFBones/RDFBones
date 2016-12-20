f301566584c47396864ecd388dbf01d9418d192b
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2016-12-20 22:42:49 +0100

    Table and TableCell classes now can return their descriptor JSON objects.

--- src/rdfbones/table/Table.java
+++ src/rdfbones/table/Table.java
@@ -3,11 +3,26 @@ package rdfbones.table;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import rdfbones.graphData.FormGraph;
+import rdfbones.lib.JSON;
 
 public class Table {
 
 	public List<TableCell> cells = new ArrayList<TableCell>();
 	public FormGraph graph;
 	
+	public JSONObject getDescriptor(){
+	
+		JSONObject object = JSON.obj();
+		//Here comes maybe later new configuration data
+    JSONArray cells = JSON.arr();
+    for(TableCell cell : this.cells){
+    	cells.put(cell.getDescriptor());
+    }
+    JSON.put(object, "cells", cells);
+    return object;
+	}
 }
--- src/rdfbones/table/TableCell.java
+++ src/rdfbones/table/TableCell.java
@@ -1,5 +1,9 @@
 package rdfbones.table;
 
+import org.json.JSONObject;
+
+import rdfbones.lib.JSON;
+
 public class TableCell {
 
 	String title;
@@ -11,5 +15,14 @@ public class TableCell {
 		this.title = title;
 		this.varName = varName;
 		this.num = num;
+	}
+	
+	public JSONObject getDescriptor(){
+		
+		JSONObject object = JSON.obj();
+    JSON.put(object, "title", this.title);
+    JSON.put(object, "dataKey", this.varName);
+    JSON.put(object, "num", this.num);
+    return object;
 	}
 }
