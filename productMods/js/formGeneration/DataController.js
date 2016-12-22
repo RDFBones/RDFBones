57bfb54aa5befc0d116281b19730bbef34bf5253
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2016-12-22 18:30:54 +0100

    New util function in DataController.js

--- productMods/js/formGeneration/DataController.js
+++ productMods/js/formGeneration/DataController.js
@@ -1,6 +1,127 @@
 
 var DataController = {
 	
+		
+	id : 0,
+	getId : function(){
+		
+		this.id++;
+		return "elementId" + this.id
+	},
+	
+	loadSubformData : function(form){
+	
+		//PopUpController.init("Loading form data")
+		var start = new Date()
+		//Only one AJAX call comes
+		dependentVars = new Object()
+		$.each(form.descriptor.formElements, function(key, formElement){
+			//Check if dependent or independent the data is
+			dataKey = DataController.getDataKey(key, formElement)
+			dependentVars[dataKey] = DataController.getInputObject(form, dataKey)
+		})
+		var toSend = {
+			editKey : editKey,
+			dependentVars : dependentVars,
+		}
+		if(Object.keys(dependentVars).length > 0){
+			$.ajax({
+				type : 'POST',
+				context : this,
+				dataType : 'json',
+				url : baseUrl + "formDataLoader",
+				data : "requestData=" + JSON.stringify(toSend)})
+				.done((function(msg) {
+					form.init(this.prepare(msg))
+				}).bind(this))
+		} else {
+			form.init()
+		}
+	},
+
+	loadSubFormDataAll : function(form, inputList){
+		
+		var dependentVars = new Object()
+		$.each(form.descriptor.formElements, function(key, formElement){
+			//Check if dependent or independent the data is
+			if(key != form.dataKey){
+				dataKey = DataController.getDataKey(key, formElement)
+				dependentVars[dataKey] = DataController.getInputObject(form.parentForm, dataKey)
+			} else {
+				dependentVars[dataKey] = inputList
+			}
+		})
+
+		var toSend = {
+			editKey : editKey,
+			arrayKey : form.dataKey,
+			dependentVars : dependentVars,
+		}
+		toSend[form.dataKey] = inputList
+		if(Object.keys(dependentVars).length > 0){
+			$.ajax({
+				type : 'POST',
+				context : this,
+				dataType : 'json',
+				url : baseUrl + "formDataLoader",
+				data : "requestData=" + JSON.stringify(toSend)})
+				.done((function(msg) {
+					form.initAll(msg)
+				}).bind(this))
+		} else {
+			form.init()
+		}
+	},
+	
+	getInputObject : function(form, dataKey){
+		
+		var msgObject = new Object()
+		$.each(dataDependencies[dataKey], function(i, variable){
+			if(form.dataObject[variable] !== undefined){
+				msgObject[variable] = form.dataObject[variable]
+			} else {
+				parentContainer = form.parentForm
+				while(true){
+					if(parentContainer !== undefined){
+						if(parentContainer.dataObject !== undefined){
+							if(parentContainer.dataObject[variable] !== undefined){
+								msgObject[variable] = parentContainer.dataObject[variable]
+								break
+							} else {
+								parentContainer = parentContainer.parentForm
+							}
+						} else {
+							parentContainer = parentContainer.parentForm
+						}
+					} else {
+						console.log("Variable is not found")
+						break
+					}		
+				} 
+			}
+		})
+		return msgObject
+	},
+
+	prepare : function(msg){
+		var dataFromAJAX = new Object()
+		$.each(msg, function(key, array){
+			dataFromAJAX[key] = DataController.getObjectFormArray(array)
+		})
+		return dataFromAJAX
+	},
+	
+	getObjectFormArray : function(arr){
+		var obj = new Object()
+		$.each(arr, function(i, element){
+			obj[element.uri] = new Object()
+			if(element.label !== undefined){
+				obj[element.uri].label = element.label
+			}
+		})
+		return obj
+	},
+
 	/*
 	 * This function only retrieves the data from the JavaScript 
 	 * does not call any JSON
@@ -62,118 +183,6 @@ var DataController = {
 		}
 	},
 	
-	loadSubformData : function(form){
-	
-		//PopUpController.init("Loading form data")
-		var start = new Date()
-		//Only one AJAX call comes
-		dependentVars = new Object()
-		$.each(form.descriptor.formElements, function(key, formElement){
-			//Check if dependent or independent the data is
-			dataKey = DataController.getDataKey(key, formElement)
-			dependentVars[dataKey] = DataController.getInputObject(form, dataKey)
-		})
-		var toSend = {
-			editKey : editKey,
-			dependentVars : dependentVars,
-		}
-		if(Object.keys(dependentVars).length > 0){
-			$.ajax({
-				type : 'POST',
-				context : this,
-				dataType : 'json',
-				url : baseUrl + "formDataLoader",
-				data : "requestData=" + JSON.stringify(toSend)})
-				.done((function(msg) {
-					form.init(this.prepare(msg))
-				}).bind(this))
-		} else {
-			form.init()
-		}
-	},
-
-	loadSubFormDataAll : function(form, inputList){
-		
-		var dependentVars = new Object()
-		$.each(form.descriptor.formElements, function(key, formElement){
-			//Check if dependent or independent the data is
-			if(key != form.dataKey){
-				dataKey = DataController.getDataKey(key, formElement)
-				dependentVars[dataKey] = DataController.getInputObject(form.parentForm, dataKey)
-			} else {
-				dependentVars[dataKey] = inputList
-			}
-		})
-
-		var toSend = {
-				editKey : editKey,
-				arrayKey : form.dataKey,
-				dependentVars : dependentVars,
-			}
-		toSend[form.dataKey] = inputList
-		if(Object.keys(dependentVars).length > 0){
-			$.ajax({
-				type : 'POST',
-				context : this,
-				dataType : 'json',
-				url : baseUrl + "formDataLoader",
-				data : "requestData=" + JSON.stringify(toSend)})
-				.done((function(msg) {
-					form.initAll(msg)
-				}).bind(this))
-		} else {
-			form.init()
-		}
-	},
-	
-	prepare : function(msg){
-		var dataFromAJAX = new Object()
-		$.each(msg, function(key, array){
-			dataFromAJAX[key] = DataController.getObjectFormArray(array)
-		})
-		return dataFromAJAX
-	},
-	
-	getObjectFormArray : function(arr){
-		var obj = new Object()
-		$.each(arr, function(i, element){
-			obj[element.uri] = new Object()
-			if(element.label !== undefined){
-				obj[element.uri].label = element.label
-			}
-		})
-		return obj
-	},
-	
-	getInputObject : function(form, dataKey){
-		
-		var msgObject = new Object()
-		$.each(dataDependencies[dataKey], function(i, variable){
-			if(form.dataObject[variable] !== undefined){
-				msgObject[variable] = form.dataObject[variable]
-			} else {
-				parentContainer = form.parentForm
-				while(true){
-					if(parentContainer !== undefined){
-						if(parentContainer.dataObject !== undefined){
-							if(parentContainer.dataObject[variable] !== undefined){
-								msgObject[variable] = parentContainer.dataObject[variable]
-								break
-							} else {
-								parentContainer = parentContainer.parentForm
-							}
-						} else {
-							parentContainer = parentContainer.parentForm
-						}
-					} else {
-						console.log("Variable is not found")
-						break
-					}		
-				} 
-			}
-		})
-		return msgObject
-	}
 }
 
 var AJAX = {
@@ -187,6 +196,15 @@ var AJAX = {
 		
 		formDescriptor : function(){
 			return AJAX.request("formDescriptor")
+		},
+		
+		
+		formGraphData : function(array, key){
+		
+			return AJAX.requestObject("formGraphData", {
+				instanceArray : array,
+				key : key,
+			})
 		},
 		
 		request : function(task){
