545e94bc7580deb0fa0ee3723be4e427971b5750
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2017-01-10 14:47:11 +0100

    ExistingFormDataLoader class were deleted (consequently the url mapping as well). The AJAXController overtakes its task.

--- src/rdfbones/formProcessing/AJAXController.java
+++ src/rdfbones/formProcessing/AJAXController.java
@@ -1,52 +1,71 @@
 package rdfbones.formProcessing;
 
+import java.util.Iterator;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import rdfbones.form.FormConfiguration;
 import rdfbones.graphData.FormGraph;
+import rdfbones.graphData.Graph;
+import rdfbones.graphData.VariableDependency;
 import rdfbones.lib.JSON;
 
 public class AJAXController {
 
-	//Input
+	// Input
 	static String TASK = "task";
 	static String KEY = "key";
 	static String INSTANCEARRAY = "instanceArray";
-
-	//Output
+	static String INPUTPARAMETERS = "inputParameters";
+	static String SUBJECTURI = "subjectUri";
+	static String OBJECTURI = "objectUri";
+	
+	// Output
 	static String TABLEDATA = "tableData";
 	public JSONObject response = new JSONObject();
 	JSONObject requestData;
-	
-	public AJAXController(FormConfiguration formConfig, JSONObject requestData){
+
+	public AJAXController(FormConfiguration formConfig, JSONObject requestData) {
 		setResponse(formConfig, requestData);
 	}
-	
-	public void setResponse(FormConfiguration formConfig, JSONObject requestData){
 
+	public void setResponse(FormConfiguration formConfig, JSONObject requestData) {
+
+		Graph graph = formConfig.dataGraph;
 		this.requestData = requestData;
 		if (check(TASK)) {
 			switch (get(TASK)) {
-			
+
 			case "formDescriptor":
-				JSON.put(this.response,"formDescriptor",formConfig.form.getFormDescriptor());
-		    JSON.put(this.response, "dataDependencies", formConfig.dataGraph.dependencyDescriptor());
+				JSON.put(this.response, "formDescriptor",
+						formConfig.form.getFormDescriptor());
+				JSON.put(this.response, "dataDependencies",
+						formConfig.dataGraph.dependencyDescriptor());
 				break;
 			case "formGraphData":
-				if (check(KEY) && check(INSTANCEARRAY)) {
+				if (check(KEY)) {
 					if (formConfig.formGraphs.containsKey(get(KEY))) {
-						FormGraph graph = formConfig.formGraphs.get(get(KEY));
-						graph.debug();
-						response(TABLEDATA, graph.getTableData(getJSONArray(INSTANCEARRAY)));
+						//FormGraph formGraph = formConfig.formGraphs.get(get(KEY));
+						JSONObject inputParameters = getJSONObject(INPUTPARAMETERS);
+						System.out.println(inputParameters.toString());
+						VariableDependency varDep = graph.variableDependencies.get(get(KEY));
+						System.out.println(graph.variableDependencies.keySet());
+						JSONArray response = varDep.getData(inputParameters);
+						response(TABLEDATA, response);
 					} else {
 						AJAXError.noFormGraphKey(response, get(KEY));
 					}
 				}
 				break;
-			case "dependentData" : 
-				break;
+			case "existingFormGraphData" :
 				
+				graph.getExistingData(get(SUBJECTURI), get(OBJECTURI));
+				response(JSON.getJSON(graph.existingData, 0));
+				break;
+		
+			case "dependentData":
+				break;
 
 			default:
 				AJAXError.unknownTask(response, get(TASK));
@@ -67,12 +86,20 @@ public class AJAXController {
 		}
 	}
 
-	public void response(String key, Object value){
-		JSON.put(this.response, key, value);
+	public void response(JSONObject value) {
+		this.response = value;
 	}
 	
+	public void response(String key, Object value) {
+		JSON.put(this.response, key, value);
+	}
+
 	public String get(String key) {
 		return JSON.string(this.requestData, key);
+	}
+
+	public JSONObject getJSONObject(String key) {
+		return JSON.get(this.requestData, key);
 	}
 
 	public JSONArray getJSONArray(String key) {
