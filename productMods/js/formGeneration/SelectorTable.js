6ce0270a630fd1d248febdf3aeba5ca562e37b09
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2017-01-18 22:26:02 +0100

    SelectorTable generates the tables so that it cares about the already added instances.

--- productMods/js/formGeneration/SelectorTable.js
+++ productMods/js/formGeneration/SelectorTable.js
@@ -4,14 +4,13 @@ var SelectorTable = function(instanceBrowser, array, descriptor){
 	
 	this.array = array
 	this.instanceBrowser = instanceBrowser
+	this.instanceSelector = instanceBrowser.instanceSelector
+	this.dataKey = instanceBrowser.instanceSelector.descriptor.dataKey
 	this.descriptor = descriptor
-	if(descriptor.type == "navigator"){
-		this.itemToAdd = NavigationItem 
-		this.check = false
-	} else {
-		this.itemToAdd = DataItem 
+	if(this.instanceSelector instanceof EditInstanceSelector){
 		this.check = true
 	}
+	
 	this.container = html.div("selectorTableContainer")
 	this.initElements()
 }
@@ -20,11 +19,28 @@ SelectorTable.prototype = {
 		
 	initElements : function(){
 		
-		arr = []
-		$.each(this.array, (function(index, value){
-			arr.push(new this.itemToAdd(this, value, this.descriptor.table.cells).container)
-		}).bind(this))
-		this.container.append(arr)
+		if(this.descriptor.type == "navigator"){
+			arr = []
+			$.each(this.array, (function(index, value){
+				arr.push(new NavigationItem(this, value, this.descriptor.table.cells).container)
+			}).bind(this))
+			this.container.append(arr)
+		} else {
+			arr = []
+			if(this.check == true){
+				$.each(this.array, (function(index, value){
+					var dataItem = new DataItem(this, value, this.descriptor.table.cells)
+					if(this.instanceSelector.addedKeys.indexof(value[this.dataKey]) == -1){
+						arr.push(dataItem.container)	
+					}
+				}).bind(this))
+			} else {
+				$.each(this.array, (function(index, value){
+					arr.push(new DataItem(this, value, this.descriptor.table.cells).container)	
+				}).bind(this))
+			}
+			this.container.append(arr)
+		}
 	},
 	
 	//This is called by the NavigationItem
@@ -43,17 +59,32 @@ SelectorTable.prototype = {
 	}
 }
 
-
-
-var SelectedTable = function(instanceBrowser, array, descriptor){
+var EditSelectorTable = function(instanceBrowser, array, descriptor){
 	
 	SelectorTable.call(this, instanceBrowser, array, descriptor)
+	this.instanceSelector = instanceBrowser.instanceSelector;
 }
 
-SelectedTable.prototype = Object.call(SelectorTable.prototype)
-
-SelectedTable.prototype.select = function(){
-	//Do nothing
-}
+EditSelectorTable.prototype = $.extend(Object.create(SelectorTable.prototype), {
+	
+	getContainer : function(){
+		
+	},
+	
+	remove : function(dataItem){
+		
+		this.instanceSelector.removeExisting 
+	},
+	
+	initElements : function(){
+		
+		arr = []
+		$.each(this.array, (function(index, value){
+			arr.push(new this.itemToAdd(this, value, this.descriptor.table.cells).container)
+		}).bind(this))
+		this.container.append(arr)
+	},
+	
+})
 
 
