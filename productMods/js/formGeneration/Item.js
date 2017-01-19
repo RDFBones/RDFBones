b228009b0b2c8bfe6e661aef1bc18814517389cd
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2017-01-19 14:53:11 +0100

    Item is in the new class definition style

--- productMods/js/formGeneration/Item.js
+++ productMods/js/formGeneration/Item.js
@@ -1,45 +1,44 @@
 
-var NavigationItem = function(table, data, cells){
+class NavigationItem {
 	
-	this.table = table
-	this.data = data
-	this.cells = cells	
-	var buttonId = util.getNewButtonId()
-	this.container = html.div("itemContainer").attr("id", buttonId)
-	$(document).on("click", "#" + buttonId, (this.select).bind(this))
-	this.init()
-}
+	constructor(table, data, cells){
+		this.table = table
+		this.data = data
+		this.cells = cells	
+		var buttonId = util.getNewButtonId()
+		this.container = html.div("itemContainer").attr("id", buttonId)
+		$(document).on("click", "#" + buttonId, (this.select).bind(this))
+		this.init()
+	}
 
-NavigationItem.prototype = {
-		
-	init : function(){
+	init (){
 		
 		if(this.cells.length == 1){
-			label = this.data[this.cells[0].dataKey]
+			var label = this.data[this.cells[0].dataKey]
 			this.container.append(html.div("singleCell").text(label))
 		} else {
-			cells = []
+			var cells = []
 			$.each(this.cells, (function(i, cell) {
 				cells.push(new ElementMap[cell.type](this.data, cell).container)
 			}).bind(this))
 			this.container.append(cells)
 		}
-	},
+	}
 	
-	select : function(){
+	select (){
 		this.table.navigate(this.data)
 	}
 }
 
-var DataItem = function(table, data, cells){
+class DataItem extends NavigationItem {
+	
+	constructor(table, data, cells){
 
-	NavigationItem.call(this, table, data, cells)	
-	this.selected = false
-}
+		super(table, data, cells)
+		this.selected = false
+	}
 
-DataItem.prototype = $.extend(Object.create(NavigationItem.prototype),
-{
-	select : function(){
+	select (){
 		
 		if(!this.selected){
 			this.table.select(this)
@@ -48,5 +47,27 @@ DataItem.prototype = $.extend(Object.create(NavigationItem.prototype),
 			this.table.remove(this)
 			this.selected = false
 		}
-	}	
-})
+	}
+
+	addToTable (){
+		this.table.container.append(this.container)
+	}
+}
+
+class SelectedDataItem extends DataItem {
+
+	constructor(table, data, cells){
+		
+		super(table, data, cells)
+	}
+	
+	select (){
+		this.table.remove(this)
+		this.container.remove()
+	}
+	
+}
+
+
+
+
