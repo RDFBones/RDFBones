28a3a9ec3452b1b3308780667e4aba1d62343fd5
kdaveed <konkolydavid@Konkolys-MBP.fritz.box>
2017-03-18 11:03:39 +0100

    InstanceRestrictionTypeTriple was renamed to InstanceRestrictionTriple. TripleUtil is not needed any more.

--- src/rdfbones/rdfdataset/InstanceTypeRestrictionTriple.java
+++
@@ -1,9 +0,0 @@
-package rdfbones.rdfdataset;
-
-public class InstanceTypeRestrictionTriple extends Triple{
-
-  public InstanceTypeRestrictionTriple(Object subject, String predicate, Object object){
-    
-    super(subject, predicate, object);
-  }
-}
