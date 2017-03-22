b3cfadde2ca8384cde0cf16f953b8f7d0dff32ba
kdaveed <konkolydavid@Konkolys-MBP.fritz.box>
2017-03-22 15:52:59 +0100

    New classes - AuxNode, InstanceRestrictionTriple

---
+++ src/rdfbones/rdfdataset/InstanceRestrictionTriple.java
@@ -0,0 +1,9 @@
+package rdfbones.rdfdataset;
+
+public class InstanceRestrictionTriple extends Triple{
+
+  public InstanceRestrictionTriple(Object subject, String predicate, Object object){
+    
+    super(subject, predicate, object);
+  }
+}
