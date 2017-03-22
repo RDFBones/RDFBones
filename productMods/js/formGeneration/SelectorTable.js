25f3bded22c60838ede4b09a5ea0751509efcad7
kdaveed <konkolydavid@Konkolys-MBP.fritz.box>
2017-03-22 20:15:31 +0100

    SelectorTable due to the removal of the navigator feauture is largerly simplified.

--- productMods/js/formGeneration/SelectorTable.js
+++ productMods/js/formGeneration/SelectorTable.js
@@ -3,113 +3,40 @@ class SelectorTable {
 	constructor(instanceBrowser, array, descriptor){
 		this.array = array
 		this.instanceBrowser = instanceBrowser
-		this.instanceSelector = instanceBrowser.instanceSelector
-		this.dataKey = instanceBrowser.instanceSelector.descriptor.dataKey
+		this.dataKey = instanceBrowser.dataKey
 		this.descriptor = descriptor
-		if(this.instanceSelector instanceof EditInstanceSelectorWindow){
-			this.check = true
-		}
-		this.initElements()
+		this.initUI()
 	}
 
-	initElements (){
-		
+	initUI (){
 		this.container = html.div("selectorTableContainer")
-		var arr = []
-		if(this.descriptor.type == "navigator"){
-			this.addNavigationItems(arr)
-		} else {
-			if(this.check == true){
-				this.addDataItemsCheck(arr)
-			} else {
-				this.addDataItems(arr)
-			}
-		}
-		this.container.append(arr)
-	}
-	
-	addNavigationItems(arr){
-		$.each(this.array, (function(index, value){
-			arr.push(new NavigationItem(this, value, this.descriptor.table.cells).container)
-		}).bind(this))
-	}
-	
-	addDataItemsCheck(arr){
-		arr.push(new TableTitle(this.descriptor.table.cells).container)
-		$.each(this.array, (function(index, value){
-			var dataItem = new DataItem(this, value, this.descriptor.table.cells)
-			if(this.instanceSelector.notSelected(dataItem)){
-				arr.push(dataItem.container)	
-			}
-		}).bind(this))
-	}
-	
-	addDataItems(arr){
-		arr.push(new TableTitle(this.descriptor.table.cells).container)
-		$.each(this.array, (function(index, value){
-			arr.push(new DataItem(this, value, this.descriptor.table.cells).container)	
-		}).bind(this))
-	}
-	
-	
-	//This is called by the NavigationItem
-	navigate (dataObject){
-		this.instanceBrowser.navigate(dataObject, this.descriptor)
-	}
-	
-	//This is called by the DataItem 
-	select (dataItem){
-		this.instanceSelector.select(dataItem)
-	}
-	
-	remove (dataItem){
-		this.instanceSelector.remove(dataItem)
-		this.container.append(dataItem.container)
-	}
-}
-
-class EditSelectorTable extends SelectorTable { 
-
-	getContainer (){
-		
-	}
-	
-	remove (dataItem){
-		this.instanceSelector.remove(dataItem)
-	} 
-
-	initElements (){
-		
-		arr = []
-		$.each(this.array, (function(index, value){
-			arr.push(new this.itemToAdd(this, value, this.descriptor.table.cells).container)
-		}).bind(this))
-		this.container.append(arr)
-	}
-}
-
-class SelectedTable { 
-	
-	constructor(instanceSelector, array, descriptor){
-		
-		this.array = array
-		this.instanceSelector = instanceSelector
-		this.dataKey = descriptor.dataKey
-		this.descriptor = descriptor
+		this.arr = []
 		this.initElements()
+		this.container.append(this.arr)
 	}
 	
-	remove(dataItem){
-		this.instanceSelector.remove(dataItem)
+	initElements (){
+		this.arr.push(new TableTitle(this.descriptor.cells).container)
+		$.each(this.array, (function(index, value){
+			var dataItem = new DataItem(this, value, this.descriptor.cells)
+			var uri = value[this.dataKey]
+			this.instanceBrowser.elementCache[uri] = dataItem
+			this.arr.push(dataItem.container)	
+		}).bind(this))
 	}
+}
+
+class SelectedTable extends SelectorTable{  
 	
 	initElements(){
-		this.container = html.div("selectorTableContainer")
-		arr = []
+
 		$.each(this.array, (function(index, value){
-			arr.push(new SelectedDataItem(this, value, this.descriptor.table.cells).container)
+			this.arr.push(new SelectedDataItem(this, value, this.descriptor.cells).container)
 		}).bind(this))
-		this.container.append(arr)
+	}
+	
+	remove (){
+		this.instanceBrowser.removeForeing()
 	}
 	
 }
